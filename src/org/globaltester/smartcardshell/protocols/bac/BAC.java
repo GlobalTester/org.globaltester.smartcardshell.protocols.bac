35414d2e03a4cb5943e18152c04ad58ad6a260e4
Alexander May <alexander.may@hjp-consulting.com>
2012-02-12 02:37:57 +0000

    implementation for BAC and SecureMessaging
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell.protocols.bac/trunk@490 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/smartcardshell/protocols/bac/BAC.java
@@ -0,0 +1,166 @@
+package org.globaltester.smartcardshell.protocols.bac;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+
+import org.globaltester.logging.logger.TestLogger;
+import org.globaltester.smartcardshell.protocols.securemessaging.Crypto;
+
+import de.cardcontact.tlv.HexString;
+
+public class BAC {
+	
+	private MRZ mrz;
+	
+	private final int SECRETLENGTH = 32;
+	
+	private final int MACLENGTH = 8;
+
+	private SecretKey kEnc;
+	private SecretKey kMac;
+	
+	private SecretKey skEnc;
+	private SecretKey skMac;	
+	
+	public BAC(){
+		java.security.Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+	}
+	
+	public void setMRZ(MRZ mrz){
+		this.mrz = mrz;
+	}
+	
+	public String getMRZ(){
+		return mrz.toString();
+	}
+	
+	public SecretKey getKenc(){
+		return kEnc;
+	}
+
+	public SecretKey getKmac(){
+		return kMac;
+	}
+
+	public SecretKey getSKenc(){
+		return skEnc;
+	}
+
+	public SecretKey getSKmac(){
+		return skMac;
+	}
+	
+	public byte[] computeMutualAuthenticateData(byte[] rndIFD, byte[] rndICC, byte[] kIFD) {
+		
+		/*
+		 * Create the input s for the cryptogram and the MAC
+		 */
+		byte[] s = new byte[SECRETLENGTH];
+		System.arraycopy(rndIFD, 0, s, 0, 8);
+		System.arraycopy(rndICC, 0, s, 8, 8);
+		System.arraycopy(kIFD, 0, s, 2 * 8, 16);
+
+		byte[] e = Crypto.computeCryptogram(s, kEnc, 1);
+		System.out.println("E_IFD: "+HexString.hexifyByteArray(e));
+		
+		byte[] m = Crypto.computeMAC(e, kMac); 
+		System.out.println("M_IFD: "+HexString.hexifyByteArray(m));
+
+		int len = e.length + m.length;
+		byte[] data = new byte[len];
+		System.arraycopy(e, 0, data, 0, e.length);
+		System.arraycopy(m, 0, data, e.length, m.length);
+		
+		return data;
+	}
+	
+	public byte[] getKicc(byte[] response) {
+		/*
+		 * decrypt message and return kicc
+		 */
+		byte[] y = new byte[SECRETLENGTH];
+		System.arraycopy(response, 0, y, 0, SECRETLENGTH);
+		y = Crypto.computeCryptogram(y, kEnc, Cipher.DECRYPT_MODE);
+		byte[] kicc = new byte[16];
+		System.arraycopy(y, 16, kicc, 0, 16);
+		return kicc;
+	}
+
+	
+	
+	public void deriveMrzKeys(){
+		
+		kEnc = Crypto.deriveKey(mrz.computeKeySeed(), (byte) 01);
+		System.out.println("Key Enc: "+HexString.hexifyByteArray(kEnc.getEncoded()));
+		
+		kMac = Crypto.deriveKey(mrz.computeKeySeed(), (byte) 02);
+		System.out.println("Key Mac: "+HexString.hexifyByteArray(kMac.getEncoded()));
+		
+	}
+	
+	public void deriveSessionKeys(byte[] keySeed){
+		
+		skEnc = Crypto.deriveKey(keySeed, (byte) 1);
+		System.out.println("SK Enc: "+HexString.hexifyByteArray(skEnc.getEncoded()));
+
+		skMac = Crypto.deriveKey(keySeed, (byte) 2);
+		System.out.println("SK Mac: "+HexString.hexifyByteArray(skMac.getEncoded()));
+
+	}
+	
+	
+	
+	public boolean verifyMutualAuthenticateResponse(byte[] response, SecretKey kmac) {
+
+		byte[] cryptogram = new byte[SECRETLENGTH];
+		System.arraycopy(response, 0, cryptogram, 0, SECRETLENGTH);
+		byte[] macMA = new byte[MACLENGTH];
+		System.arraycopy(response, SECRETLENGTH, macMA, 0, MACLENGTH);
+
+		byte[] checksum = Crypto.computeMAC(cryptogram, kmac);
+		if (!HexString.hexifyByteArray(macMA).equalsIgnoreCase(
+				HexString.hexifyByteArray(checksum))) {
+			System.out.println("MAC expected: "
+					+ HexString.hexifyByteArray(checksum));
+			System.out.println("MAC received: " + HexString.hexifyByteArray(macMA));
+			System.out.println("Error while generating checksum");
+			return false;
+		}
+		System.out.println("MAC of Mutual Authenticate Response: " +HexString.hexifyByteArray(macMA));
+		return true;
+	}
+	
+	public byte[] computeKeySeed(byte[] kIFD, byte[] kICC){
+		// Calculate XOR of kIFD and kICC:
+		byte[] keySeed = new byte[16];
+		for (int i = 0; i < 16; i++) {
+			keySeed[i] = (byte) ((kIFD[i] & 0xFF) ^ (kICC[i] & 0xFF));
+		}
+		System.out.println("Key Seed: "+HexString.hexifyByteArray(keySeed));
+		return keySeed;
+	}
+
+	public static byte[] getRandomBytes(int numberOfBytes){
+		byte[] rndBytes = new byte[numberOfBytes];
+		
+		try {
+			SecureRandom rng = SecureRandom.getInstance("SHA1PRNG");
+			rng.nextBytes(rndBytes);
+		} catch (NoSuchAlgorithmException e1) {
+			TestLogger.error(e1);
+		}
+		return rndBytes;
+	}
+	
+	public static byte[] calculateInitialSendSequenceCounter(byte[] rndICC, byte[] rndIFD){
+		
+		byte[] ssc = new byte[8];
+		System.arraycopy(rndICC, 4, ssc, 0, 4);
+		System.arraycopy(rndIFD, 4, ssc, 4, 4);
+		
+		return ssc;
+	}
+}
--- src/org/globaltester/smartcardshell/protocols/bac/ProtocolProvider.java
+++ src/org/globaltester/smartcardshell/protocols/bac/ProtocolProvider.java
@@ -8,6 +8,20 @@ import org.globaltester.smartcardshell.protocols.ScshCommandParameter;
 
 public class ProtocolProvider extends AbstractScshProtocolProvider {
 
+	private static ScshCommand selectApplicationEPASS;
+	{
+		selectApplicationEPASS = new ScshCommand("selectApplicationEPASS");
+		selectApplicationEPASS.setHelp("Select the ePassport application");
+		selectApplicationEPASS.setHelpReturn("");
+
+		String impl = "";
+		impl += "var cmd = new ByteString(\"00 A4 04 0C 07 A0 00 00 02 47 10 01\", HEX);\n";
+		impl += "card.gt_sendCommand(cmd);\n";
+		impl += "assertStatusWord(SW_NoError, card.SW.toString(HEX));\n";
+		selectApplicationEPASS.setImplementation(impl);
+	}
+	
+	
 	private static ScshCommand performBAC;
 	{
 		performBAC = new ScshCommand("performBAC");
@@ -20,13 +34,53 @@ public class ProtocolProvider extends AbstractScshProtocolProvider {
 		performBAC.addParam(mrzParam);
 
 		String impl = "";
-		impl += "print(\"perform BAC\")\n";
+		impl += "print(\"perform BAC wit MRZ \" + mrz)\n";
+		
+		impl += "var bac = this.gt_BAC_getBAC();\n";
+		impl += "bac.setMRZ(new Packages.org.globaltester.smartcardshell.protocols.bac.MRZ(mrz));\n";
+		impl += "bac.deriveMrzKeys();\n";
+		impl += "\n";
+		impl += "var rndICC = this.gt_ISO7816_getChallenge(8);\n";
+		impl += "var rndIFD = bac.getRandomBytes(8);\n";
+		impl += "var kIFD = bac.getRandomBytes(16);\n";
+		impl += "\n";
+		impl += "var mutualAuthData = bac.computeMutualAuthenticateData(rndIFD, rndICC, kIFD);\n";
+		impl += "var mutualAuthResp = this.gt_ISO7816_mutualAuthenticate(mutualAuthData);\n";
+		impl += "\n";
+		impl += "var kICC = bac.getKicc(mutualAuthResp);\n";
+		impl += "\n";
+		impl += "var keySeed = bac.computeKeySeed(kIFD, kICC);\n";
+		impl += "bac.deriveSessionKeys(keySeed);\n";
+		impl += "\n";
+		impl += "var sKenc = bac.getSKenc();\n";
+		impl += "var sKmac = bac.getSKmac();\n";
+		impl += "var ssc = bac.calculateInitialSendSequenceCounter(rndICC, rndIFD);\n";
+		impl += "\n";
+		impl += "this.gt_SecureMessaging_initSM(sKenc, sKmac, ssc);\n";
+		
 		performBAC.setImplementation(impl);
+	}
+	
+	private static ScshCommand getBAC;
+	{
+		getBAC = new ScshCommand("getBAC");
+		getBAC.setHelp("Return the BAC object associated with this card. This can be used to manipulate the SM bevahior of sendCommand() and sendSM()\n\nThis method behaves like a singleton access method, e.g. if the SM instance does not exist it will be created but several sequential calls to this method will always return the same instance.");
+		getBAC.setHelpReturn("BAC object used by this card (Instance of org.globaltester.smartcardshell.protocols.bac.BAC");
+
+		String impl = "";
+		impl += "if (this.gt_BAC_BAC == undefined) {\n";
+		impl += "    print(\"gt_BAC_BAC is not defined yet, will be created now\");\n";
+		impl += "    this.gt_BAC_BAC = new Packages.org.globaltester.smartcardshell.protocols.bac.BAC()";
+		impl += "}\n";
+		impl += "return this.gt_BAC_BAC;\n";
+		getBAC.setImplementation(impl);
 	}
 
 	@Override
 	public void addCommands(List<ScshCommand> commandList) {
+		commandList.add(getBAC);
 		commandList.add(performBAC);
+		commandList.add(selectApplicationEPASS);
 	}
 
 }
